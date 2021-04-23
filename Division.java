57230cc3b33bfe565ed8cb9a1690aed23942e457
Bruno <brunoandia007@gmail.com>
2021-04-23 16:06:00 +0200

    Actualizado

---
+++ Division.java
@@ -0,0 +1,9 @@
+public class Division {
+    public static void main(String[] args) {
+    int numero1=10;
+    int numero2=2;
+    
+    int resultado = numero1/numero2;
+    System.out.printf("La división es " + numero1 + " / " + numero2 + " = " + resultado);
+    }
+}
